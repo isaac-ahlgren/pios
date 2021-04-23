bd464b761b06199fd3f5a90c73bde28981fa1bdc
isaac-ahlgren <isaacahlgren12@gmail.com>
2021-04-22 21:28:59 -0500

    updated

--- src/elf.c
+++ src/elf.c
@@ -85,6 +85,8 @@ bool check_elf64_hdr(Elf64_Ehdr* hdr) {
     return true;
 }       
 
+
+// I DONT THING I NEED THIS
 bool elf_load_allocated(Elf64_Ehdr* hdr, FILE* exec, struct ppage** pages) {
 
     Elf64_Shdr shdr[10]; // temporary buffer for section headers
@@ -95,7 +97,7 @@ bool elf_load_allocated(Elf64_Ehdr* hdr, FILE* exec, struct ppage** pages) {
     for (int i = 0; i < hdr->e_shnum; i++) {
     
         // if section header buffer empty or all read, read more
-        if (i % 10) {
+        if (!(i % 10)) {
             read_file(exec, (unsigned char*) shdr, sizeof(Elf64_Shdr) * 10);
         }    
         
@@ -156,7 +158,7 @@ bool elf_load_segments(Elf64_Ehdr*hdr, FILE* exec, struct ppage** pages) {
     for (int i = 0; i < hdr->e_phnum; i++) {
     
         // if program header buffer empty or all read, read more
-        if (i % 10) { 
+        if (!(i % 10)) { 
             read_file(exec, (unsigned char*) phdr, sizeof(Elf64_Phdr) * 10);
         }    
    
@@ -176,7 +178,7 @@ bool elf_load_segments(Elf64_Ehdr*hdr, FILE* exec, struct ppage** pages) {
 	    // allocate and map necissary pages
 	    for (int i = 0; i < pages_needed; i++) {
         
-		// allocate page    
+		// allocate page
                 new_page = allocate_physical_pages(1);
                 
 		// check if page was allocated
@@ -189,15 +191,15 @@ bool elf_load_segments(Elf64_Ehdr*hdr, FILE* exec, struct ppage** pages) {
 	        mapPages(vaddr, new_page->physical_addr);
 
                 // read needed bytes into page
-		unsigned int bytes_needed = (i*PAGE_SIZE - prog_hdr->p_filesz > 0) ? PAGE_SIZE % prog_hdr->p_filesz : PAGE_SIZE; 
+                unsigned int bytes_needed = (i*PAGE_SIZE - prog_hdr->p_filesz > 0) ? PAGE_SIZE % prog_hdr->p_filesz : PAGE_SIZE;
 	        read_file(exec, (unsigned char*) vaddr, bytes_needed);
-
-		// set difference between memsize and filesize to 0
-		memset(vaddr + prog_hdr->p_filesz, 0, prog_hdr->p_memsz - prog_hdr->p_filesz);	
                 
 		// next vaddr for next page
 	        vaddr += PAGE_SIZE;
             }
+
+            // set difference between memsize and filesize to 0
+            memset(vaddr + prog_hdr->p_filesz, 0, prog_hdr->p_memsz - prog_hdr->p_filesz);
             
 	    // return offset to previous
 	    set_offset(exec, offset);
